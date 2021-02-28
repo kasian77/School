5e4c0e4c66d082fb48459c3c9e3e95b08e3cb367
kasian77 <79701896+kasian77@users.noreply.github.com>
2021-02-28 14:20:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php
-$n = rand(1, 13);
-switch ($n){
-    case 1: echo "Учим буквы"; break;
-    case 2: echo "Учим таблицу умножения"; break;
-    case 3: echo "Учим 3"; break;
-    case 4: echo "Учим 4"; break;
-    case 5: echo "Учим 5"; break;
-    case 6: echo "Учим 6"; break;
-    case 7: echo "Учим 7"; break;
-    case 8: echo "Учим 8"; break;
-    case 9: echo "Учим 9"; break;
-    case 10: echo "Учим 10"; break;
-    case 11: echo "Учим 11"; break;
-    case 12: echo "Учим 12"; break;
-    default: echo "Такого класса у нас нет!"; }
-    ?>
+		$p = 3.1415926535898;
+		$r = rand(100, 500)
+      ?>
+      	<div style="background-color: red; width: <?php echo "{$r}px" ?>; height: <?php echo "{$r}px" ?>; border-radius: 50%;"></div>
+	<?php
+	?>
