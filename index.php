210b7dc061cd3ddef687f53c69a5968e33280658
kasian77 <79701896+kasian77@users.noreply.github.com>
2021-02-28 12:48:40 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+switch ($n){
+    case 1: echo "Учим буквы"; break;
+    case 2: echo "Учим таблицу умножения"; break;
+    case 3: echo "Учим 3"; break;
+    case 4: echo "Учим 4"; break;
+    case 5: echo "Учим 5"; break;
+    case 6: echo "Учим 6"; break;
+    case 7: echo "Учим 7"; break;
+    case 8: echo "Учим 8"; break;
+    case 9: echo "Учим 9"; break;
+    case 10: echo "Учим 10"; break;
+    case 11: echo "Учим 11"; break;
+    case 12: echo "Учим 12"; break;
+    default: echo "Такого класса у нас нет!"; }
+    ?>
